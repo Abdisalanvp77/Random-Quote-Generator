8287654483b943cedefb0bec1b1fa92fe90e1045
Abdisalan Abdi <abdisalan_abdi@cooperators.ca>
2020-04-07 23:39:24 -0400

    Updates

---
+++ precache-manifest.d4be31cba87e271db3706bcad8426ff2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d81d1fe8c74feb6781de16de8a2f78",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "d540322b393a3e47050d",
+    "url": "/Random-Quote-Generator/static/css/2.af8e9aa6.chunk.css"
+  },
+  {
+    "revision": "26188abc775dc6afac2c",
+    "url": "/Random-Quote-Generator/static/css/main.197d3fd3.chunk.css"
+  },
+  {
+    "revision": "d540322b393a3e47050d",
+    "url": "/Random-Quote-Generator/static/js/2.4247f4f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Random-Quote-Generator/static/js/2.4247f4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26188abc775dc6afac2c",
+    "url": "/Random-Quote-Generator/static/js/main.8935ea95.chunk.js"
+  },
+  {
+    "revision": "021c984653ecb9a3e4eb",
+    "url": "/Random-Quote-Generator/static/js/runtime-main.8278c5c7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Random-Quote-Generator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Random-Quote-Generator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Random-Quote-Generator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Random-Quote-Generator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Random-Quote-Generator/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
